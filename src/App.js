bb6140e4d4d28471dcacc78fa4c4b23557a8db06
minhoo03 <minhoo0333@gmail.com>
2021-06-08 00:40:21 +0900

    Nav CSS

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ import IntroPage from './components/IntroPage'
 import ProductPage from './components/ProductPage'
 import CartPage from './components/CartPage'
 
+import LoginPage from './items/LoginPage'
+import RegisterPage from './items/RegisterPage'
+
 function App() {
   return (
     <div className="container">
       <Router>
         <Nav />
-        
+
         <Switch>
             <Route exact path="/" component={MainPage} />
             <Route exact path="/introduct" component={IntroPage} />
             <Route exact path="/product" component={ProductPage} />
             <Route exact path="/my-cart" component={CartPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/register" component={RegisterPage} />
         </Switch>
       </Router>
     </div>
--- src/items/Nav.js
+++ src/items/Nav.js
@@ -1,29 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
 export default function Nav() {
+
+    const [test, setTest] = useState(false)
+
     return (
-        <div style={{display:'flex'}}>
-                <div>
-                    <nav>
-                        <ul>
+        <>
+            <nav style={{ width: '100%', height: '100px', position:'relative' }}>
+                <ul style={{ width:'500px', height:'100px', display: 'flex', 
+                            justifyContent:'space-around', alignItems:'center', 
+                            position: 'absolute', right:'8%' }}>
+                    <li>
+                        <Link className="li-Link" to="/">HOME</Link>
+                    </li>
+                    <li>
+                        <Link className="li-Link" to="/introduct">ABOUT US</Link>
+                    </li>
+                    <li>
+                        <Link className="li-Link" to="/product">PRODUCT</Link>
+                    </li>
+
+                    {test === false ?                         
+                        <>
                             <li>
-                                <Link to="/">HOME</Link>
+                                <Link className="li-Link" to="/login">LOGIN</Link>
                             </li>
                             <li>
-                                <Link to="/introduct">ABOUT US</Link>
+                                <Link className="li-Link" to="/register">REGISTER</Link>
+                            </li>
+                        </>
+                        :
+                        <>
+                            <li className="li-Link">
+                                LOGOUT
                             </li>
                             <li>
-                                <Link to="/product">PRODUCT</Link>
+                                <Link className="li-Link" to="/my-cart">MY CART</Link>
                             </li>
-                            <li>
-                                <Link to="/my-cart">MY CART</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-        </div>
+                        </>
+                    }
+                    
+                    
+                </ul>
+            </nav>
+        </>
 
         
     )
