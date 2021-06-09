5cd02632fdcee8ac64161d688ec14932bbf66ffd
minhoo03 <minhoo0333@gmail.com>
2021-06-09 09:58:36 +0900

    Use react-hook-form:RegisterPage

--- src/items/RegisterPage.js
+++ src/items/RegisterPage.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 
 export default function RegisterPage() {
+
+    const { register, watch, formState:{errors}, handleSubmit } = useForm({mode: 'onChange'})
+    console.log(watch('email', 'name'));
+    const onSubmit = data => console.log(data)
+
     return (
-        <div>
-            RegisterPage
+        <div className="auth_wrapper">
+            <h3>Register</h3>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <lable>Email</lable>
+                <input type="email" {...register('email', { required: true, pattern: /^\S+@\S+$/i}) }/>
+                {errors.email && errors.email.type === 'required' && <p>Email field is required</p>}
+
+                <lable>Name</lable>
+                <input type="text" name="name" {...register('name', { required: true, maxLength: 10 })} />
+                {errors.name && errors.name.type === 'required' && <p>This field is required.</p>}
+                {errors.name && errors.name.type === 'maxLength' && <p>Your input exceed maximum length.</p>}
+                
+                <input type="submit" />
+            </form>
+
         </div>
     )
 }
